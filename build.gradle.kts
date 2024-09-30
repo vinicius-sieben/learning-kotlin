2aeed3c955e3cebafa1d2bd8f6e68737b7b9b6f2
Vinícius Sieben <84821880+vinicius-sieben@users.noreply.github.com>
2024-09-30 19:40:40 -0300

    Add: Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
